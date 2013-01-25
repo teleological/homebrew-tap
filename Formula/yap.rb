491d614f41544de48d353aa22de560b19ad6a5f3
Riley <bx+github@teleological.net>
2013-01-25 13:05:45 -0500

    Fixed build, added threads and java
    * Fixed issue by disabling MYDDAS_ODBC flag
    * Fixed issue by requiring readline and passing path
    * Added enable-clpbn-bp option (per Paulo Moura, >v6.2 may not need)
    * Removed enable-or-parallelism option (breaks v6.3 build)
    * Added enable-threads and enable-pthread-locking options
    * Added with-java option
    * Added devel version 6.3.3

--- Formula/yap.rb
+++ Formula/yap.rb
@@ -2,10 +2,17 @@ require 'formula'
 
 class Yap < Formula
   homepage 'http://www.dcc.fc.up.pt/~vsc/Yap/index.html'
+
   url 'http://www.dcc.fc.up.pt/~vsc/Yap/yap-6.2.2.tar.gz'
   sha1 'a02f80cac67c287645b2ced9502f5ea24a07f1c3'
 
+  devel do
+    url 'http://www.dcc.fc.up.pt/~vsc/Yap/yap-6.3.3.tar.gz'
+    sha1 'd191e419e5cf74b11e003aae5fe148f3f2f26ac5'
+  end
+
   depends_on 'gmp'
+  depends_on 'readline'
 
   fails_with :clang do
     build 421
@@ -15,12 +22,23 @@ class Yap < Formula
   end
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--enable-threads",
-                          "--enable-pthread-locking",
-                          "--enable-or-parallelism",
-                          "--with-gmp=#{Formula.factory('gmp').opt_prefix}"
-                          "--prefix=#{prefix}"
+    system "./configure",
+      "--disable-debug", "--disable-dependency-tracking",
+                         "--enable-tabling",
+                         "--enable-depth-limit",
+                         "--enable-coroutining",
+                         "--enable-threads",
+                         "--enable-pthread-locking",
+                         "--enable-clpbn-bp=no",
+                         "--with-gmp=#{Formula.factory('gmp').opt_prefix}",
+                         "--with-readline=#{Formula.factory('readline').opt_prefix}",
+                         "--with-java=/Library/Java/Home",
+                         "--prefix=#{prefix}"
+
+    inreplace 'Makefile' do |s|
+      s.gsub! /-DMYDDAS_ODBC/, ''
+    end
+
     system "make"
     system "make install"
   end
